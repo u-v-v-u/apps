194a2642096225f440d74769a17e1724f3e781ef
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2022-12-18 16:46:20 +1100

    feat(database): Use prisma

---
+++ apps/api/src/plugins/prisma.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from 'database'
+import fp from 'fastify-plugin'
+
+declare module 'fastify' {
+  interface FastifyInstance {
+    prisma: PrismaClient;
+  }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default fp(async (fastify, _opts) => {
+  const prisma = new PrismaClient()
+
+  await prisma.$connect()
+
+  fastify.decorate('prisma', prisma)
+
+  fastify.addHook('onClose', async (fastify) => {
+    await fastify.prisma.$disconnect()
+  })
+})
