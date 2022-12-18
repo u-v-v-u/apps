d4e7cf9bc9e462957a479f62397d03dba708b5ac
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2022-12-18 21:29:32 +1100

    chore(web): Convert signup form into a component

--- apps/web/src/components/index.ts
+++ apps/web/src/components/index.ts
@@ -1,1 +1,2 @@
-export { default as NavBar } from './NavBar.svelte';
+export { default as NavBar } from './NavBar.svelte'
+export { default as SignupForm } from './SignupForm.svelte'
--- apps/web/src/routes/signup/+page.svelte
+++ apps/web/src/routes/signup/+page.svelte
@@ -1,41 +1,10 @@
 <script lang="ts">
-  import { reporter, ValidationMessage } from '@felte/reporter-svelte';
-  import { createForm } from 'felte';
-  import { validator } from '@felte/validator-zod';
-  import * as zod from 'zod';
-
-  const formSchema = zod.object({
-    email: zod.string().email().nonempty(),
-    password: zod.string().nonempty()
-  });
-
-  const { form } = createForm({
-    onSubmit: (values) => {
-      console.log(values);
-    },
-    validate: validator({ formSchema }),
-    extend: reporter
-  });
+  import { SignupForm } from '@components'
 </script>
 
 <div class="flex h-screen justify-center items-center">
   <div class="container flex justify-center items-center">
-    <form use:form class="flex flex-col justify-center items-center">
-      <input type="email" name="email" placeholder="Email" />
-
-      <ValidationMessage for="email" let:messages={message}>
-        <span class="form-input-title">{message || ''}</span>
-        <!-- <span slot="placeholder">Enter a Valid Email</span> -->
-      </ValidationMessage>
-
-      <input type="password" name="password" placeholder="Password" />
-
-      <ValidationMessage for="password" let:messages={message}>
-        <span class="form-input-title">{message || ''}</span>
-      </ValidationMessage>
-
-      <button type="submit" id="formButton">Sign Up</button>
-    </form>
+    <SignupForm />
   </div>
 </div>
 
@@ -47,39 +16,5 @@
     width: 50vh;
 
     @apply rounded;
-  }
-
-  span {
-    font-weight: bold;
-    font-size: 1rem;
-    color: white;
-  }
-
-  input {
-    margin: 1rem;
-    width: 250px;
-    height: 50px;
-    padding: 1rem;
-
-    @apply rounded;
-  }
-
-  button {
-    margin-top: 20px;
-    padding: 10px;
-    padding-left: 50px;
-    padding-right: 50px;
-    border: 1px solid;
-    border-color: white;
-    transition: background-color 0.3s;
-
-    color: white;
-
-    @apply rounded;
-
-    &:hover {
-      color: black;
-      background-color: white;
-    }
   }
 </style>
