3b0962187c4b08eeb39d37b5f86839d80ba0ab13
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2022-12-18 12:33:26 +1100

    Initial app

---
+++ apps/web/src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+  import '../app.scss';
+</script>
+
+<div class="prose prose-zinc">
+  <slot />
+</div>
---
+++ apps/web/src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<div class="w-screen h-screen flex flex-col justify-center items-center">
+  <h1>Hello, World!</h1>
+</div>
